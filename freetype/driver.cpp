b298658fcbb6e99f93f1e703a251b0c09fd18d62
Yoshinori Sano <yoshinori.sano@gmail.com>
2022-09-30 21:54:46 +0900

    Dump outline

--- freetype/driver.cpp
+++ freetype/driver.cpp
@@ -1,7 +1,7 @@
 #include "ft2build.h"
 #include FT_FREETYPE_H
 
-int main(void)
+int dump_bitmap(void)
 {
     FT_Library library = nullptr;
     FT_Face face = nullptr;
@@ -60,4 +60,91 @@ free:
 	}
 
     return error;
+}
+
+#include "freetype/ftoutln.h"
+
+int move_to(const FT_Vector* to, void* user)
+{
+	printf("Move to (%ld, %ld)\n", to->x, to->y);
+	return 0;
+}
+
+int line_to(const FT_Vector* to, void* user)
+{
+	printf("Line to (%ld, %ld)\n", to->x, to->y);
+	return 0;
+}
+
+int conic_to(const FT_Vector* control, const FT_Vector* to, void* user)
+{
+	printf("Conic to (%ld, %ld) with control (%ld, %ld)\n", to->x, to->y, control->x, control->y);
+	return 0;
+}
+
+int cubic_to(const FT_Vector* control1, const FT_Vector* control2, const FT_Vector* to, void* user)
+{
+	printf("Cubic to (%ld, %ld) with controls (%ld, %ld) and (%ld, %ld)\n", to->x, to->y, control1->x, control1->y, control2->x, control2->y);
+	return 0;
+}
+
+int dump_outline(void)
+{
+	FT_Library library = nullptr;
+	FT_Face face = nullptr;
+	FT_Error error;
+
+	error = FT_Init_FreeType(&library);
+	if (error) {
+		return error;
+	}
+
+	const char* filename = "C:\\Windows\\Fonts\\arial.ttf";
+
+	error = FT_New_Face(library, filename, 0, &face);
+	if (error) {
+		goto free;
+	}
+
+	error = FT_Set_Char_Size(face, 40 * 64, 0, 100, 0);
+	if (error) {
+		goto free;
+	}
+
+	error = FT_Load_Char(face, 'q', FT_LOAD_DEFAULT);
+	if (error) {
+		goto free;
+	}
+	FT_GlyphSlot glyph = face->glyph;
+
+	error = FT_Render_Glyph(glyph, FT_RENDER_MODE_MONO);
+	if (error) {
+		goto free;
+	}
+
+	FT_Outline outline = glyph->outline;
+
+	FT_Outline_Funcs funcs;
+	funcs.move_to = move_to;
+	funcs.line_to = line_to;
+	funcs.conic_to = conic_to;
+	funcs.cubic_to = cubic_to;
+
+	FT_Outline_Decompose(&outline, &funcs, nullptr);
+
+free:
+	if (face) {
+		FT_Done_Face(face);
+	}
+	if (library) {
+		FT_Done_FreeType(library);
+	}
+
+	return error;
+}
+
+int main(void)
+{
+	// return dump_bitmap();
+	return dump_outline();
 }
