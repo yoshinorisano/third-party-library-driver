d0346bcffb4099a38462290e804c74e6753ad0c6
Yoshinori Sano <yoshinori.sano@gmail.com>
2022-11-02 07:12:47 +0900

    Do cxx tutorial: Until "Calling a Rust function from C++"

--- rust-cxx/cxx-demo/include/blobstore.h
+++ rust-cxx/cxx-demo/include/blobstore.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <memory>
 
+struct MultiBuf;
+
 class BlobstoreClient {
 public:
     BlobstoreClient();
+    uint64_t put(MultiBuf &buf) const;
 };
 
 std::unique_ptr<BlobstoreClient> new_blobstore_client();
